3346494115b9b1c46063595a4f5739ad8786c5af
joseluisrosasbaza <Bazno379!>
2015-08-13 15:04:56 -0500

    First draft of the site

--- res/js/menu.js
+++ res/js/menu.js
@@ -2,7 +2,7 @@ function scrollChange(){
   var yPos = window.scrollY;
 
   if(yPos > 100){
-      $("#menuBar").animate({ height:'45'}, 600);
+      $("#menuBar").animate({ height:'50'}, 600);
   }
 }
 
@@ -14,12 +14,41 @@ function scrollMenu(){
   console.log(top);
 
   if(top >= breakpoint){ //Cuando baja
-    $("#menuBar").animate({ height:'45'}, 600);
+    $("#menuBar").animate({ height:'50'}, 600);
     //console.log("Baja");
   }else{ //Cuando sube
-    $("#menuBar").animate({ height:'0'}, 600);
+    $("#menuBar").animate({ height:'80'}, 600);
     //console.log("Sube");
   }
 }
 
 //$(menu).animate({left: "-=300px"}, 300);
+
+function chText(option){
+  var cubo = document.getElementById('title-container');
+  var option = option;
+  var text = "";
+
+  if(option == 1){
+    text = '<div class="wow fadeInRight"><span class="light" style="font-size:50px; font-weight: 400;">Tenemos más de 35 años <br>de experiencia internaiconal.</span><br></div><br><div class="wow fadeIn"><img src="res/img/graph/adorno.png" alt="" /></div>';
+  }else{
+    if (option == 2) {
+      text = '<div class="wow fadeInRight"><span class="light" style="font-size:50px; font-weight: 400;">Organizamos los mejores eventos <br>para satizfacer tus necesidades.</span><br></div><br><div class="wow fadeIn"><img src="res/img/graph/adorno.png" alt="" /></div>';
+    }else {
+      if (option == 3) {
+        text = '<div class="wow fadeInRight"><span class="light" style="font-size:50px; font-weight: 400;">Lorem impsum dolor <br>dolor et sit ammet.</span><br></div><br><div class="wow fadeIn"><img src="res/img/graph/adorno.png" alt="" /></div>';
+      }else {
+        if (option == 4) {
+          text = '<div class="wow fadeInRight"><span class="light" style="font-size:50px; font-weight: 400;">Lorem impsum dolor <br>dolor et sit ammet.</span><br></div><br><div class="wow fadeIn"><img src="res/img/graph/adorno.png" alt="" /></div>';
+        }else {
+          if (option == 5) {
+            text = '<div class="wow fadeInRight"><span class="light" style="font-size:50px; font-weight: 400;">Lorem impsum dolor <br>dolor et sit ammet.</span><br></div><br><div class="wow fadeIn"><img src="res/img/graph/adorno.png" alt="" /></div>';
+          }
+        }
+      }
+    }
+  }
+  //console.log(option);
+
+  cubo.innerHTML = text;
+}
